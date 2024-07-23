77ab4429305b8b2c058ffa8a192609d5b81fedd1
Ravisankkaran  Ilangovan <ravisankkaran.i@medyaan.com>
2024-07-23 19:14:56 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">KELLY</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link pb-0" exact-active-class="active">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link pb-0" exact-active-class="active">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/resume" class="nav-link pb-0" exact-active-class="active">Resume</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/service" class="nav-link pb-0" exact-active-class="active">Services</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/portfolio" class="nav-link pb-0" exact-active-class="active">Portfolio</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/contact" class="nav-link pb-0" exact-active-class="active">Contact</router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav social-links">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fab fa-twitter"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fab fa-facebook"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fab fa-instagram"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fab fa-linkedin"></i></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+    <copyright/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import copyright from './components/CopyrightPage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    copyright
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CopyrightPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- footer component -->
+    <div id="copyright">
+        <div class=" p-lg-3">
+        <p class="m-0"> © Copyright <strong>Kelly</strong> . All Rights Reserved</p>
+        <p class="m-0">Designed by<span class="dec-text"> BootstrapMade</span> </p>
+    </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'CopyrightPage',
+};
+</script>
